0bc2600c38e585ccd4a31a998c14b60b1ff1acd7
srujang <srujan.gourishetty@hyscale.io>
2020-12-03 08:57:33 +0530

    fix for mailing

--- scripts/publish_artifacts.sh
+++ scripts/publish_artifacts.sh
@@ -19,6 +19,5 @@ aws_cp_upload()
   aws s3 cp scripts/hyscale s3://$AWS_S3_BUCKET/hyscale/release/$1/hyscale
   aws s3api put-object-tagging --bucket $AWS_S3_BUCKET  --key hyscale/release/$1/hyscale --tagging 'TagSet=[{Key=hyscalepubliccontent,Value=true}]'
 }
-
 docker_build_push
 aws_cp_upload $artifactory_version
