0d7b2febe70a2d40a64c0331bb6b3680183dde3e
nagachandra-immadisetty <nagachandra.immadisetty@hyscale.io>
2020-12-07 15:13:42 +0530

    Fix : Java Doc errors. Added module-info.java in trouble shooting module.

---
+++ deployer/troubleshooting-integration/src/main/java/module-info.java
@@ -0,0 +1,21 @@
+module troubleshooting.integration {
+    exports com.github.srujankujmar.troubleshooting.integration.actions;
+    exports com.github.srujankujmar.troubleshooting.integration.builder;
+    exports com.github.srujankujmar.troubleshooting.integration.conditions;
+    exports com.github.srujankujmar.troubleshooting.integration.constants;
+    exports com.github.srujankujmar.troubleshooting.integration.errors;
+    exports com.github.srujankujmar.troubleshooting.integration.models;
+    exports com.github.srujankujmar.troubleshooting.integration.service;
+    exports com.github.srujankujmar.troubleshooting.integration.spring;
+    exports com.github.srujankujmar.troubleshooting.integration.util;
+
+    requires spring.context;
+    requires spring.beans;
+    requires deployer.services;
+    requires deployerModel;
+    requires client.java.api;
+    requires commons;
+    requires slf4j.api;
+    requires spring.core;
+    requires java.annotation;
+}
